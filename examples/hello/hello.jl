0c662aec19373316630b32c773253fbc40162ebf
goropikari <goropikari56@gmail.com>
2021-01-29 00:52:27 +0900

    add explanation

--- examples/hello/hello.jl
+++ examples/hello/hello.jl
@@ -1,9 +1,13 @@
 import AWSLambdaRuntimeInterfaceClients
 const ric = AWSLambdaRuntimeInterfaceClients
 
+# Make handler
 function handler(event, context)
     return "Hello World!"
 end
 
+# Initialize RIC
 ric.init()
+
+# Pass handler to RIC and execute
 ric.execute(handler)
