f127a9969bc308a48749fd620c7569594cad2d07
goropikari <goropikari56@gmail.com>
2021-01-29 00:16:29 +0900

    fix header type

--- src/AWSLambdaRuntimeInterfaceClients.jl
+++ src/AWSLambdaRuntimeInterfaceClients.jl
@@ -11,16 +11,17 @@ end
 function next_invocation()
     res = HTTP.get("$(BASE_URL)/invocation/next")
     body = String(res.body)
-    headers = res.headers
+    headers = Dict(res.headers)
     return headers, body
 end
 
-function invocation_response(request_id::String)
+function invocation_response(request_id, response)
     url = "$(BASE_URL)/invocation/$(request_id)/response"
-    HTTP.post(url, body="SUCCESS")
+    headers = ["Content-type" => "application/json"]
+    HTTP.post(url, headers, response)
 end
 
-function invocation_error(request_id::String)
+function invocation_error(request_id)
     url = "$(BASE_URL)/invocation/$(request_id)/error"
     body = Dict("errorType" => "InvalidEventDataException",
                 "errorMessage" => "Error parsing event data.")
@@ -45,15 +46,15 @@ function execute(handler)
             state = :start
             headers, body = next_invocation()
             request_id = headers["Lambda-Runtime-Aws-Request-Id"]
-            @info "Received event" request_id
+            # @info "Received event" request_id
             state = :received
 
             response  = handler(body, headers)
-            @info "Got response from handler" response
+            # @info "Got response from handler" response
             state = :handled
 
-            invocation_response(request_id)
-            @info "notified lambda runtime"
+            invocation_response(request_id, response)
+            # @info "notified lambda runtime"
             state = :finished
         catch ex
             if state == :start
